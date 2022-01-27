603c60cba342703f8ef60de2d0384045f205e3dd
CHITTA RANJAN SWAIN <chittaranjan138@gmail.com>
2022-01-27 20:33:29 +0530

    Create MaximumSubArraySum.java
    Arrays #1 : Maximum subarray sum

---
+++ Arrays/MaximumSubArraySum.java
@@ -0,0 +1,20 @@
+/**
+* Given an integer array nums, find the contiguous subarray (containing at least one number)
+* which has the largest sum and return its sum.
+*
+* A subarray is a contiguous part of an array.
+
+* Leetcode problem # 53
+* Scaler Arrays assignment # 3
+*/
+class Solution {
+    public int maxSubArray(int[] nums) {
+      int sum = nums[0];
+      int max = nums[0];
+      for (int i=1; i<nums.length; i++) {
+        sum = Math.max(0, sum) + nums[i]; //Discard negative
+        max = Math.max(sum, max);
+      }
+      return max;
+    }
+}
