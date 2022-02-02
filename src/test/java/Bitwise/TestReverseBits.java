ceb8f289bd6a7a5f01963da2c7023a7813a9d4a1
CHITTA RANJAN SWAIN <chittaranjan138@gmail.com>
2022-02-02 15:28:57 +0530

    Reverse bits of 32 bit integer

---
+++ src/test/java/Bitwise/TestReverseBits.java
@@ -0,0 +1,21 @@
+package Bitwise;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestReverseBits {
+  private ReverseBits reverseBits;
+
+  @Before
+  public void init() {
+
+  }
+
+  @Test
+  public void testSuccess() {
+    long result = reverseBits.reverse(0);
+    Assert.assertEquals(3, result);
+  }
+
+}
